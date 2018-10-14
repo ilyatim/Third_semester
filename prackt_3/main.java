e9c44b65750fbed4feb83cbcd4ce474425f29cb2
ilyatim <ilyatimofeevgg@mail.ru>
2018-10-14 10:54:11 +0300

    update

--- prackt_3/main.java
+++ prackt_3/main.java
@@ -7,7 +7,36 @@ public class main
     {
         Scanner sc = new Scanner(System.in);
         double side = sc.nextDouble();
-        Square square = new Square(side);
+        Shape square_1 = new Square(side);
+        System.out.println(square_1.getColor());
+        System.out.println(square_1.getPerimetr());
+        System.out.println(square_1.isFilled());
+        String color = sc.next();
+        square_1.setColor(color);
+        System.out.println(square_1.getColor());
 
+        Rectangle rectangle_1 = (Rectangle)square_1;
+        System.out.println(rectangle_1.getLenght());
+        System.out.println(rectangle_1.getWidth());
+        System.out.println(rectangle_1.getColor());
+        System.out.println(rectangle_1.getPerimetr());
+        System.out.println(rectangle_1.isFilled());
+
+        Square square_2 = (Square) rectangle_1;
+        System.out.println(square_2.getSide());
+        System.out.println(square_2.getColor());
+        System.out.println(square_2.getPerimetr());
+        System.out.println(square_2.isFilled());
+
+        Shape circle_1 = new Circle();
+        System.out.println(circle_1.getColor());
+        System.out.println(circle_1.getPerimetr());
+        System.out.println(circle_1.isFilled());
+
+        Circle circle_2 = (Circle)circle_1;
+        System.out.println(circle_2.getColor());
+        System.out.println(circle_2.getPerimetr());
+        System.out.println(circle_2.isFilled());
+        System.out.println(circle_2.getRadius());
     }
 }
